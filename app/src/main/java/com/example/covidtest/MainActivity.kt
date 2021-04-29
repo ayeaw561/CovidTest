a9de66430c4619c368be95d93e13374d5f78a892
ayeaw561 <70350651+ayeaw561@users.noreply.github.com>
2021-04-29 18:17:43 -0400

    log in

--- app/src/main/java/com/example/covidtest/MainActivity.kt
+++ app/src/main/java/com/example/covidtest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.covidtest
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
@@ -9,10 +10,12 @@ import com.google.firebase.firestore.FirebaseFirestore
 //import kotlin.android.synthetic.main.activity_firestore.*
 
 class MainActivity : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val db = FirebaseFirestore.getInstance()
+        //val db = FirebaseFirestore.getInstance()
         val showButton = findViewById<Button>(R.id.btnLogin)
         val usrName = findViewById<EditText>(R.id.et_username)
         val usrPass = findViewById<EditText>(R.id.et_password)
@@ -21,19 +24,39 @@ class MainActivity : AppCompatActivity() {
         showButton.setOnClickListener {
 
             // Getting the user input
+            val testval = " "
+
             val text = usrName.text.toString()
             val pass = usrPass.text.toString()
             val textv = tv.text.toString()
 
-            // Showing the user input
-            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-            Toast.makeText(this, pass, Toast.LENGTH_SHORT).show()
-            val users = db.collection("Users")
-                .whereEqualTo("password", pass)
-                .get()
-            Log.d(textv, users.toString())
+            readDataFromFirestore(text, textv, pass)
+
         }
 
+    }
+
+    private fun readDataFromFirestore(text : String, textv : String, pass : String){
+
+
+        db.collection("Users").document(text)
+                .get()
+                .addOnSuccessListener { document ->
+                    try {
+                        if (document != null) {
+                            Log.d(textv, "DocumentSnapshot read successfully!")
+                            val testval = document.data?.get("testval")
+                            val password = document.data?.get("password")
+                            val intent = Intent(this, )
+                        } else {
+                            Log.d(textv, "No such document!")
+                        }
+                    }catch (ex: Exception){
+                        Log.e(textv, ex.message.toString())
+                    }
+                }.addOnFailureListener {
+                    e -> Log.e(textv, "Error writing document", e)
+                }
     }
 
 
