d7a7f36002464e43ce7a84e9c021e3b3a671d963
ayeaw561 <alexanderyeaw@gmail.com>
2021-04-30 15:15:40 -0400

    CovidInfo btn

--- app/src/main/java/com/example/covidtest/ProfileActivity.kt
+++ app/src/main/java/com/example/covidtest/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.example.covidtest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -8,6 +9,7 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile1)
+        val signupbtn = findViewById<Button>(R.id.btnSignUp)
         val testVal = findViewById<TextView>(R.id.testVal_tv)
         testVal.text = intent.getStringExtra("Test")
         val email = findViewById<TextView>(R.id.email_tv)
@@ -18,6 +20,12 @@ class ProfileActivity : AppCompatActivity() {
         last.text = intent.getStringExtra("Last")
         val zip = findViewById<TextView>(R.id.zip_tv)
         zip.text = intent.getStringExtra("Zipcode")
+
+        signupbtn.setOnClickListener {
+
+            val intent = Intent(this, CovidInfoActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
